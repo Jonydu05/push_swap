adf6f4f0cd4606f1a892cb77b10c3314849f8619
PietraoDev <pietroaDev@gmail.com>
2026-08-12 21:04:40 -0300

    Add: Current includes

--- include/push_swap.h
+++ include/push_swap.h
@@ -1,6 +1,12 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+// Unistd.h included functions read, write
+# include <unistd.h>
+
+// Stdlib.h included functions malloc, free and exit
+# include <stdlib.h>
+
 void	teste(void);
 
 #endif
