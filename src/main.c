79e3928f6fabd06b482626e77459ecc1f8ff51ea
PietraoDev <pietroaDev@gmail.com>
2026-08-13 21:22:42 -0300

    Add: Basic exemple use linkedlist in main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
 #include "push_swap.h"
+#include <stdio.h>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	teste();
+	(void)argc;
+	(void)argv;
+
+	t_linkedlist	*stack;
+
+	stack = malloc(sizeof(t_linkedlist));
+	if (!stack)
+		return (1);
+	linked_init(stack);
+	add_content(push_front(stack), 1); // tail
+	add_content(push_front(stack), 2); // middle node
+	add_content(push_front(stack), 3); // head
+	list_print(stack);
+	pop_front(stack); // 2 head
+	list_print(stack);
 	return (0);
 }
