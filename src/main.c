6ec94dc87045d3075bc0bc5135a56fcaaab3b619
PietraoDev <pietroaDev@gmail.com>
2026-08-13 23:01:54 -0300

    Alter: Make basic test of functions linked list

--- src/main.c
+++ src/main.c
@@ -12,11 +12,14 @@ int	main(int argc, char **argv)
 	if (!stack)
 		return (1);
 	linked_init(stack);
-	add_content(push_front(stack), 1); // tail
-	add_content(push_front(stack), 2); // middle node
-	add_content(push_front(stack), 3); // head
+	add_content(push_front(stack), 1); // tail: 0x1
+	add_content(push_front(stack), 2); // middle: 0x2
+	add_content(push_front(stack), 3); // head: 0x3
 	list_print(stack);
-	pop_front(stack); // 2 head
+	pop_front(stack); // head: 0x2
 	list_print(stack);
+	// Esse codigo de teste causa naturalmente 2 leaks propositas, por conta
+	// Da nao liberacao dos dois nodes via pop
+	free(stack);
 	return (0);
 }
